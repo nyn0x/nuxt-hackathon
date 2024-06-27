f5e865775d04f66ba9dc6f8459dafe080d411b53
nyn0x <nynox77@hotmail.fr>
2024-06-27 23:08:13 +0200

    test

--- app.vue
+++ app.vue
@@ -1,7 +1,21 @@
 <template>
   <div>
     <main class="container">
+    <section>
       <h1>hello</h1>
+
+      <form>
+  <fieldset role="group">
+    <input
+      type="email"
+      name="email"
+      placeholder="Todo"
+      autocomplete="email"
+    />
+    <input type="submit" value="Envoyer" />
+  </fieldset>
+</form>
+</section>
     </main>
   </div>
 </template>
