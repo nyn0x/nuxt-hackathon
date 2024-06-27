b39c9b4dd534b1cebaeef5b440a55e197adddda2
Nicolas <nicolas.pause@additi.fr>
2024-06-27 23:47:24 +0200

    save

--- app.vue
+++ app.vue
@@ -1,26 +1,62 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref<string>('')
+const messages = ref<string[]>([])
+
+const addMessage = () => {
+  if (message.value.trim()) {
+    messages.value.push(message.value)
+    message.value = ''
+  }
+}
+</script>
+
 <template>
   <div>
-    <main class="container">
-    <section>
-      <h1>hello</h1>
+    <main class="app">
+      <div class="container">
+        <article v-for="(msg, index) in messages" :key="index" class="app__card">{{ msg }}</article>
 
-      <form>
-  <fieldset role="group">
-    <input
-      type="email"
-      name="email"
-      placeholder="Todo"
-      autocomplete="email"
-    />
-    <input type="submit" value="Envoyer" />
-  </fieldset>
-</form>
-</section>
+        <form @submit.prevent="addMessage" class="app__form">
+          <fieldset role="group">
+            <input v-model="message" type="text" name="email" placeholder="Tapez un message" autocomplete="email" />
+
+            <input type="submit" value="Envoyer" />
+          </fieldset>
+        </form>
+      </div>
     </main>
   </div>
 </template>
 
 
-<style>
+<style lang="scss">
 @import url("~/assets/css/app.css");
+
+body {
+  background-color: #f0f0f0;
+  /* Couleur de fond pour mieux voir le centrage */
+}
+
+.app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  /* 100% de la hauteur de la vue */
+  margin: 0;
+  /* Enlever les marges par défaut */
+  background-color: #f0f0f0;
+  /* Couleur de fond pour mieux voir le centrage */
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+
+  &__form {
+    width: 100%;
+  }
+}
 </style>
